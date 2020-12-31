7dd56da4a5f753697ccd1dbb79ea8a3bcea695ed
Deepak Surya S <ideepaksuryas@gmail.com>
2020-12-31 11:26:13 +0530

    configure babel & upgrade packages

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,18 @@
 import express from 'express'
 import path from 'path'
 import open from 'open'
+import webpack from 'webpack'
+import config from '../webpack.config.dev'
+import webpackDevMiddleware from 'webpack-dev-middleware'
 
 const port = 3000
 const app = express()
+const compiler = webpack(config)
+
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: config.output.publicPath
+}))
+
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'))
---
+++ src/index.js
@@ -0,0 +1,2 @@
+const money = '$69,420'
+console.log(`I found ${money} on my way back home!`)
